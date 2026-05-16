d463c4f1d556eb0dbf354fd1ee15695c86887a9b
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2026-05-16 15:34:45 +1000

    feat: test AoT d:\CSharp\rainclipse\.git\modules\hypercube\modules\submodules\ecs\COMMIT_EDITMSG

---
+++ src/Hypercube.Ecs.Analyzers/Link.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+
+namespace Hypercube.Ecs.Analyzers;
+
+// NOTE: Shared?
+[PublicAPI]
+public static class Link
+{
+    public const string ComponentInterfaceFullName = "Hypercube.Ecs.Components.IComponent";
+    public const string ComponentInterfaceName = "IComponent";
+
+    public const string ComponentAttributeFullName = "Hypercube.Ecs.Attributes.ComponentAttribute";
+    public const string ComponentAttributeName = "ComponentAttribute";
+}
